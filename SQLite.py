eadc08304a0bf856e685f38831b78369b554a39d
Bessonov <Bessonov.dmitriy92@gmail.com>
2020-02-13 10:12:25 +0200

    Ver 3.0

--- SQLite.py
+++ SQLite.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect("database")
+conn = sqlite3.connect("database3")
 cursor = conn.cursor()
 
 cursor.execute('''CREATE TABLE Работник(ID integer PRIMARY KEY,
@@ -8,10 +8,10 @@ cursor.execute('''CREATE TABLE Работник(ID integer PRIMARY KEY,
 Имя text NOT NULL,
 Отчество text NOT NULL)''')
 
-cursor.execute("INSERT INTO Работник(Фамилия,Имя,Отчество) Values ('Комаров', 'Виктор','Павлович')")
+cursor.execute("INSERT INTO Работник(Фамилия,Имя,Отчество) Values ('Комаров', 'Виктор','Павлович'),"
+               "('Иванов','Петр','Васильевич')")
 conn.commit()
-cursor.execute("INSERT INTO Работник(Фамилия,Имя,Отчество) Values ('Иванов', 'Петр','Васильевич')")
-conn.commit()
+
 for row in cursor.execute('SELECT * FROM Работник'):
     print(row)
 
@@ -21,10 +21,10 @@ cursor.execute('''CREATE TABLE Зарплата(ID integer PRIMARY KEY NOT NULL,
 Выплата integer NOT NULL
 ,Остаток integer NOT NULL,FOREIGN KEY (ID) REFERENCES Работник (ID))''')
 
-cursor.execute("INSERT INTO Зарплата(Фамилия,Выплата,Остаток) Values ('Комаров', '5000','5000')")
+cursor.execute("INSERT INTO Зарплата(Фамилия,Выплата,Остаток) Values ('Комаров', '5000','5000'),"
+               "('Иванов','7000','3000')")
 conn.commit()
-cursor.execute("INSERT INTO Зарплата(Фамилия,Выплата,Остаток) Values ('Иванов', '7000','3000')")
-conn.commit()
+
 for row in cursor.execute('SELECT * FROM Зарплата'):
     print(row)
 
@@ -33,9 +33,9 @@ cursor.execute('''CREATE TABLE Должность(ID integer PRIMARY KEY NOT NUL
 Посада Text NOT NULL,
 Контракт integer NOT NULL,FOREIGN KEY (ID) REFERENCES Работник (ID))''')
 
-cursor.execute("INSERT INTO Должность(Фамилия,Посада,Контракт) Values ('Комаров', 'Консультант','104')")
+cursor.execute("INSERT INTO Должность(Фамилия,Посада,Контракт) Values ('Комаров', 'Консультант','104'),"
+               "('Иванов','Директор','27')")
 conn.commit()
-cursor.execute("INSERT INTO Должность(Фамилия,Посада,Контракт) Values ('Иванов', 'Директор','27')")
-conn.commit()
+
 for row in cursor.execute('SELECT * FROM Должность'):
     print(row)
